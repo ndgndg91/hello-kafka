38289d8fddf9cdee85c24a7a7137a96c864e5798
ndgndg91 <ndgndg91@gmail.com>
2023-06-11 22:30:32 +0900

    <feat> kafka-consumer-groups  in localhost

--- code/1-kafka-cli/4-kafka-consumer-groups.sh
+++ code/1-kafka-cli/4-kafka-consumer-groups.sh
@@ -1,38 +1,5 @@
- # Replace "kafka-consumer-groups.sh" 
-# by "kafka-consumer-groups" or "kafka-consumer-groups.bat" based on your system # (or bin/kafka-consumer-groups.sh or bin\windows\kafka-consumer-groups.bat if you didn't setup PATH / Environment variables)
-
-
-############################
-### CONDUKTOR PLATFORM #####
-############################ 
-
-
-# documentation for the command 
-kafka-consumer-groups.sh 
-
-# list consumer groups
-kafka-consumer-groups.sh --command-config playground.config --bootstrap-server cluster.playground.cdkt.io:9092 --list
- 
-# describe one specific group
-kafka-consumer-groups.sh --command-config playground.config --bootstrap-server cluster.playground.cdkt.io:9092 --describe --group my-second-application
-
-# describe another group
-kafka-consumer-groups.sh --command-config playground.config --bootstrap-server cluster.playground.cdkt.io:9092 --describe --group my-first-application
-
-# start a consumer
-kafka-console-consumer.sh --consumer.config playground.config --bootstrap-server cluster.playground.cdkt.io:9092 --topic first_topic --group my-first-application
-
-# describe the group now
-kafka-consumer-groups.sh --command-config playground.config --bootstrap-server cluster.playground.cdkt.io:9092 --describe --group my-first-application
-
-# describe a console consumer group (change the end number)
-kafka-consumer-groups.sh --command-config playground.config --bootstrap-server cluster.playground.cdkt.io:9092 --describe --group console-consumer-10592
-
-# start a console consumer
-kafka-console-consumer.sh --consumer.config playground.config --bootstrap-server cluster.playground.cdkt.io:9092 --topic first_topic --group my-first-application
-
-# describe the group again
-kafka-consumer-groups.sh --command-config playground.config --bootstrap-server cluster.playground.cdkt.io:9092 --describe --group my-first-application
+ # Replace "kafka-consumer-groups" 
+# by "kafka-consumer-groups" or "kafka-consumer-groups.bat" based on your system # (or bin/kafka-consumer-groups or bin\windows\kafka-consumer-groups.bat if you didn't setup PATH / Environment variables)
 
 
 ############################
@@ -41,28 +8,28 @@ kafka-consumer-groups.sh --command-config playground.config --bootstrap-server c
 
 
 # documentation for the command 
-kafka-consumer-groups.sh 
+kafka-consumer-groups 
 
 # list consumer groups
-kafka-consumer-groups.sh --bootstrap-server localhost:9092 --list
+kafka-consumer-groups --bootstrap-server localhost:9092 --list
  
 # describe one specific group
-kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group my-second-application
+kafka-consumer-groups --bootstrap-server localhost:9092 --describe --group my-second-application
 
 # describe another group
-kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group my-first-application
+kafka-consumer-groups --bootstrap-server localhost:9092 --describe --group my-first-application
 
 # start a consumer
-kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic first_topic --group my-first-application
+kafka-console-consumer --bootstrap-server localhost:9092 --topic first_topic --group my-first-application
 
 # describe the group now
-kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group my-first-application
+kafka-consumer-groups --bootstrap-server localhost:9092 --describe --group my-first-application
 
 # describe a console consumer group (change the end number)
-kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group console-consumer-10592
+kafka-consumer-groups --bootstrap-server localhost:9092 --describe --group console-consumer-10592
 
 # start a console consumer
-kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic first_topic --group my-first-application
+kafka-console-consumer --bootstrap-server localhost:9092 --topic first_topic --group my-first-application
 
 # describe the group again
-kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group my-first-application
+kafka-consumer-groups --bootstrap-server localhost:9092 --describe --group my-first-application
