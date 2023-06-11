863ac9b3a7ac678214508b2bdcd7ba264d6360e3
ndgndg91 <ndgndg91@gmail.com>
2023-06-11 21:59:16 +0900

    <fix> start kafka sh

--- code/0-start-kafka/macosx/macosx-start-kafka-kraft.sh
+++ code/0-start-kafka/macosx/macosx-start-kafka-kraft.sh
@@ -16,15 +16,16 @@ mkdir data/kafka-kraft
 
 
 # generate a Kafka UUID
-kafka-storage.sh random-uuid
+kafka-storage random-uuid
 
-# This returns a UUID, for example 76BLQI7sT_ql1mBfKsOk9Q
-kafka-storage.sh format -t <uuid> -c ~/kafka_2.13-3.1.0/config/kraft/server.properties
+# This returns a UUID, for example 7Xqs5LXlTUCFwCPyb8xY3g
+kafka-storage format -t <uuid> -c /usr/local/etc/kafka/kraft/server.properties
+#Formatting /usr/local/var/lib/kraft-combined-logs with metadata.version 3.4-IV0.
 
 # This will format the directory that is in the log.dirs in the config/kraft/server.properties file
 
 # start Kafka
-kafka-server-start.sh ~/kafka_2.13-3.1.0/config/kraft/server.properties
+kafka-server-start /usr/local/etc/kafka/kraft/server.properties
 
 # Kafka is running! 
 # Keep the terminal window opened
