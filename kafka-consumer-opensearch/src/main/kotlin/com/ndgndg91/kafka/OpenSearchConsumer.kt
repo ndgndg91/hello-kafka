b9277eb6266111ddfd379e1e2d3b6238f16884e5
ndgndg91 <ndgndg91@gmail.com>
2024-10-27 16:09:33 +0900

    <feat> enhance consumer

--- kafka-consumer-opensearch/src/main/kotlin/com/ndgndg91/kafka/OpenSearchConsumer.kt
+++ kafka-consumer-opensearch/src/main/kotlin/com/ndgndg91/kafka/OpenSearchConsumer.kt
@@ -16,10 +16,14 @@ import org.opensearch.client.indices.GetIndexRequest
 import org.opensearch.common.xcontent.XContentType
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.node.ObjectNode
+import org.apache.kafka.common.errors.WakeupException
 import org.opensearch.action.bulk.BulkRequest
+import org.slf4j.LoggerFactory
 import java.net.URI
 import java.time.Duration
 import java.util.*
+
+val logger = LoggerFactory.getLogger("openSearchConsumer")
 
 fun main() {
     // first create on OpenSearch Client
@@ -58,36 +62,44 @@ fun main() {
             openSearchClient.indices().create(createIndexRequest, RequestOptions.DEFAULT)
         }
 
-        // subscribe topic
-        consumer.subscribe(listOf("wikimedia.recentchange"))
+        try {
+            // subscribe topic
+            consumer.subscribe(listOf("wikimedia.recentchange"))
 
-        while (true) {
-            val records = consumer.poll(Duration.ofMillis(3000))
-            val recordCount = records.count()
-            val bulkRequest = BulkRequest()
+            while (true) {
+                val records = consumer.poll(Duration.ofMillis(3000))
+                val recordCount = records.count()
+                val bulkRequest = BulkRequest()
 
-            println("Received $recordCount records")
-            records.forEach {
-                val jsonData = removeLogParams(it.value())
-                // 멱등 컨슈머 구현
-                // 1. id 정의 kafka record coordinates 를 통해서 topic - partition - offset
-                // 2. data 에 id 가 있는 경우 이를 그대로 사용한다.
-                val indexRequest = IndexRequest("wikimedia")
-                    .source(jsonData.second, XContentType.JSON)
-                    .id(jsonData.first)
-                bulkRequest.add(indexRequest)
+                println("Received $recordCount records")
+                records.forEach {
+                    val jsonData = removeLogParams(it.value())
+                    // 멱등 컨슈머 구현
+                    // 1. id 정의 kafka record coordinates 를 통해서 topic - partition - offset
+                    // 2. data 에 id 가 있는 경우 이를 그대로 사용한다.
+                    val indexRequest = IndexRequest("wikimedia")
+                        .source(jsonData.second, XContentType.JSON)
+                        .id(jsonData.first)
+                    bulkRequest.add(indexRequest)
+                }
+
+                if (bulkRequest.numberOfActions() > 0) {
+                    val response = openSearchClient.bulk(bulkRequest, RequestOptions.DEFAULT)
+                    println("${response.items.size} record processed")
+
+                    // commit offset after batch is consumed.
+                    consumer.commitSync()
+                    println("offset committed")
+                }
             }
-
-            if (bulkRequest.numberOfActions() > 0) {
-                val response = openSearchClient.bulk(bulkRequest, RequestOptions.DEFAULT)
-                println("${response.items.size} record processed")
-
-                // commit offset after batch is consumed.
-                consumer.commitSync()
-                println("offset committed")
-            }
-
-
+        } catch (e: WakeupException) {
+            logger.info("Consumer is starting to shut down")
+        } catch (e: Exception) {
+            logger.error("Unexpected exception in the consumer", e)
+        } finally {
+            consumer.close() // close the consumer, this will also commit offsets
+            openSearchClient.close()
+            logger.info("The consumer is now gracefully shut down")
         }
     }
 
